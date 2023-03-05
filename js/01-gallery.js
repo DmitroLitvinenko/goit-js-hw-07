e2a8aabcb09e56232d5c0466557f24d262e46bf6
sides1 <dima.litvinenko1990@gmail.com>
2023-03-05 14:42:33 +0200

    complete gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+
+const galleryContainer = document.querySelector('.gallery');
+const modalContentElement = document.querySelector('.modal');
+
+const imgElements = galleryItems.map( ({original, preview, description} )  => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </div>`}).join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', imgElements);
+
+
+galleryContainer.addEventListener( 'click', onGalleryElementClick)
+
+function onGalleryElementClick(event) {
+    event.preventDefault();
+    
+    const target =event.target
+    if(target.nodeName !== 'IMG') {
+        return;
+}
+    const originalImageURL = target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img src=${originalImageURL} >
+        `)
+        instance.show()
+  };
+
+
+
 console.log(galleryItems);
